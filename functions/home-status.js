eb8101916bd724b7109e2d5e31e33e5f9e818beb
jasenmichael <jasen@ready-builder.com>
2020-02-10 18:47:53 -0500

    deploy

---
+++ functions/home-status.js
@@ -0,0 +1,121 @@
+/* eslint-disable eqeqeq */
+/* eslint-disable space-before-function-paren */
+/* eslint-disable no-console */
+const axios = require('axios')
+const moment = require('moment-timezone')
+require('dotenv').config()
+
+const {
+  JANDA_DB_URL,
+  HOME_STATUS_ID,
+  HOME_STATUS_KEY
+} = process.env
+
+exports.handler = function (event, context, cb) {
+  console.log(context)
+  const URL = `${JANDA_DB_URL}/${HOME_STATUS_ID}`
+  const options = {
+    headers: {
+      'Content-Type': 'application/json',
+      'cache-control': 'no-cache',
+      'x-apikey': HOME_STATUS_KEY
+    }
+  }
+  const checkConnection = (status) => {
+    axios.get(URL, options)
+      .then(async (res) => {
+        // const online = false
+        if ((res.data.online && await checkIfUpdated(res.data.next_check)) || status === true) {
+          const result = {
+            'online': res.data.online,
+            'next_check': res.data.next_check,
+            'last_check': res.data.last_check
+          }
+          send(result, 200)
+        } else {
+          const result = {
+            'online': false,
+            'next_check': res.data.next_check,
+            'last_check': res.data.last_check
+          }
+          send(result, 404)
+        }
+      })
+      .catch((err) => {
+        console.error(err)
+      })
+  }
+
+  const send = (body, statusCode) => {
+    cb(null, {
+      statusCode,
+      headers: {
+        'content-type': 'application/json',
+        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Headers': 'Origin, X-Requested-With, Content-Type, Accept'
+      },
+      body: JSON.stringify(body)
+    })
+  }
+
+  function checkIfUpdated(nextCheck) {
+    console.log('checking if updated in last 15 minutes')
+    const startTime = moment().tz('America/Chicago').format()
+    const date = startTime.split('T')[0]
+    const nowHr = startTime.split('T')[1].split(':')[0]
+    let nCheckDate = date
+    const nCheck = nextCheck.replace(' am', '').replace(' pm', '').split(':')
+    let nCheckHr = Number(nCheck[0]) <= 9 ? `${0}${Number(nCheck[0])}` : Number(nCheck[0])
+    if (nextCheck.includes('pm')) {
+      nCheckHr = Number(nCheck[0]) + 12
+    }
+    const nCheckMin = Number(nCheck[1]) <= 9 ? `${0}${Number(nCheck[1])}` : Number(nCheck[1])
+    const nCheckSec = Number(nCheck[2]) <= 9 ? `${0}${Number(nCheck[2])}` : Number(nCheck[2])
+
+    if (nowHr == 23 && nCheckHr == 12) {
+      nCheckHr = '00'
+      nCheckDate = moment(nCheckDate, 'YYYY-MM-DD').add('days', '1').format().split('T')[0]
+    }
+    const nextCheckTime = `${nCheckDate}T${nCheckHr}:${nCheckMin}:${nCheckSec}-06:00`
+    console.log(nextCheckTime)
+    console.log(startTime)
+    const minutesTillNextUpdate = moment.utc(moment(nextCheckTime).diff(moment(startTime))).format('mm')
+
+    console.log('minutesTillNextUpdate', minutesTillNextUpdate)
+    if (minutesTillNextUpdate >= 16) {
+      postOffline()
+      return false
+    } else {
+      return true
+    }
+    // return true
+  }
+
+  function postOffline() {
+    axios.put(URL, {
+      online: false
+    }, {
+      headers: {
+        'Content-Type': 'application/json',
+        'cache-control': 'no-cache',
+        'x-apikey': HOME_STATUS_KEY
+      }
+    }).catch(err => console.error(err))
+  }
+
+  if (event.httpMethod === 'GET') {
+    if (event.queryStringParameters.resource === HOME_STATUS_ID) {
+      checkConnection(false)
+    } else {
+      checkConnection(true)
+    }
+  } else {
+    cb(null, {
+      statusCode: 400,
+      headers: {
+        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Headers': 'Origin, X-Requested-With, Content-Type, Accept'
+      }
+    })
+  }
+}
---
+++ pages/index.vue
@@ -0,0 +1,54 @@
+<template>
+  <div class="container">
+    <h2>Janda Home Server Status</h2>
+    <b-button block size="lg" disabled>
+      {{ status }}
+    </b-button>
+    <h6 class="text-center">
+      <b>Last Check:</b>
+      {{ last }}
+    </h6>
+    <h6 class="text-center">
+      <b>Next Check:</b>
+      {{ next }}
+    </h6>
+  </div>
+</template>
+
+<script>
+/* eslint-disable arrow-parens */
+/* eslint-disable space-before-function-paren */
+/* eslint-disable no-console */
+import axios from 'axios'
+export default {
+  data() {
+    return {
+      status: 'loading..',
+      last: '...',
+      next: '...'
+    }
+  },
+  mounted() {
+    console.log(
+      'mounted',
+      `http://${process.env.NETLIFY_FUNCTIONS_URL}/home-status`
+    )
+    setTimeout(() => {
+      this.status = this.status + '.'
+    }, 750)
+    setTimeout(() => {
+      this.status = this.status + '.'
+      axios(`http://${process.env.NETLIFY_FUNCTIONS_URL}/home-status`)
+        .then(res => {
+          console.log(res.data)
+          this.status = res.data.online === true ? 'Online' : 'Offline'
+          this.last = res.data.last_check
+          this.next = res.data.next_check
+        })
+        .catch(err => {
+          console.error(err)
+        })
+    }, 1500)
+  }
+}
+</script>
